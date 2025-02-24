ea34f6c23ca2698dc65f38f8897d9a7bf812fd99
Goni1999 <gonigoni667@gmail.com>
2025-02-24 02:08:34 +0100

    Initial commit

--- src/components/referral/Referral.tsx
+++ src/components/referral/Referral.tsx
@@ -20,13 +20,13 @@ const Referral = () => {
     <div className="flex flex-col lg:flex lg:flex-row justify-between p-5 gap-5 lg:gap-10">
       {/* Left Side */}
       <div className="w-full lg:w-[2/3]">
-        <h2 className="text-4xl text-gray-500 dark:text-gray-400 font-semibold leading-snug md:text-5xl lg:text-6xl">
+        <h2 className="text-3xl text-gray-500 dark:text-gray-400 font-semibold leading-snug md:text-5xl lg:text-6xl">
           Refer Friends.
         <br/>
           Get 100 USD Equivalent Trading Fee Credit Each.
         </h2>
       </div>
-
+    <br/>
       {/* Right Side (Form) */}
       <div className="w-full lg:w-[1/3] border border-gray-300 rounded-lg p-5">
         <div className="mb-5">
