6f06a7ec1439a06f97b5b1461c2e8a5049106864
Goni1999 <gonigoni667@gmail.com>
2025-03-10 03:18:28 +0100

    mm

--- src/components/users/UserTable.tsx
+++ src/components/users/UserTable.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from "react";
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "../ui/table";
 import Badge from "../ui/badge/Badge";
